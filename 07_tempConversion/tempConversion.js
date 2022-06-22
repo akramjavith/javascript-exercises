79fbddc1ba2543ca5e1b1d1e1941078beba4d6cd
javith <akramjavithkhan@gmail.com>
2022-06-22 17:25:49 +0530

    temp

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
 const ftoc = function() {
-
+  let fahrenheit=32;
+  let ftoc=Math.floor(fahrenheit-32)*5/9;
+  return ftoc;
 };
 
 const ctof = function() {
+  let celcius = 0;
+ let ctof = Math.floor(celcius*9/5)+32;
+  return ctof;
 
 };
 
