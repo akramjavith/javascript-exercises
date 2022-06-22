f39d613ed5139bb50db5398e8a5d45ed0a02b520
javith <akramjavithkhan@gmail.com>
2022-06-22 18:07:42 +0530

    palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    return str == str.split("").reverse().join("");
+    
 };
+alert(("malayalam")); 
+alert(("english"));
+ 
+
 
 // Do not edit below this line
 module.exports = palindromes;
