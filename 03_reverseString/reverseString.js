0f1f4ab31a804358f7cca385ee68618fda63fa8f
javith <akramjavithkhan@gmail.com>
2022-06-22 16:28:29 +0530

    reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
 const reverseString = function() {
-
+    const revString='hello';
+  return revString.split("").reverse().join("");
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
