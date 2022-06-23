de80a3b6160659a728a36d5f9b593897592b2ced
javith <akramjavithkhan@gmail.com>
2022-06-23 10:26:07 +0530

    titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const books = [
+        {
+          title: 'Book1',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      ]
+      return array.map(book => book.title);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
 const findTheOldest = function() {
-
+    var year_born = prompt("Please enter your date of birth:", "Type here");
+var d = new Date();
+var n = d.getFullYear();
+function getAge(birthYear){
+	var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    age = currentYear - birthYear;
+    return age;
+}
+calculatedAge = getAge(year_born);
+alert("Hello, " + "you are " + calculatedAge + " years old!");
 };
 
 // Do not edit below this line
